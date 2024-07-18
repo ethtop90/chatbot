7b143214a9bf73ec7337087f79035aa7e8a4209e
ethtop90 <fullsuccess.world@gmail.com>
2024-07-18 16:43:59 +0900

    modified: serve static file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,10 +5,14 @@ from flask_jwt_extended import JWTManager
 from datetime import timedelta
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 app.config.from_object('app.config')
 app.config["JWT_SECRET_KEY"] = "your_jwt_secret_key"
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)  # Set the token to expire in 1 day
+
+@app.errorhandler(404)
+def not_found(e):
+  return app.send_static_file('index.html')
 
 
 client = MongoClient(app.config['MONGO_URI'])
