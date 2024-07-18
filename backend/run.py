b4c7fe236f6de563cd87d5abe831076dc9370f86
ethtop90 <fullsuccess.world@gmail.com>
2024-07-18 11:16:16 +0900

    modified: correct run.py

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,11 @@
 from app import app
 from app.utils.llm import prepare_llm
-from flup.server.fcgi import WSGIServer
+from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
     # app.run(host='0.0.0.0', port=8080, debug=True)
-    WSGIServer(app, bindAddress=('0.0.0.0', 8080), certfile='cert.pem', keyfile='key.pem').run()
+    # http_server = WSGIServer(('0.0.0.0', 8080), app, keyfile='key.pem', certfile='cert.pem')
+    # http_server.serve_forever()
+
     prepare_llm()
