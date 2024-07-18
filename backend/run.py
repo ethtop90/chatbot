87f49c9b1a07c4bd7114ceb5a5577cdd80069891
ethtop90 <fullsuccess.world@gmail.com>
2024-07-18 17:06:55 +0900

    modified: run.py

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,7 @@ from app.utils.llm import prepare_llm
 from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'key.pem'))
     # app.run(host='0.0.0.0', port=8080, debug=True)
     # http_server = WSGIServer(('0.0.0.0', 8080), app, keyfile='key.pem', certfile='cert.pem')
     # http_server.serve_forever()
