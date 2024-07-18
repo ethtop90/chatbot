ec95da476b464fc2b9ee63051666e97a8be8c9d7
ethtop90 <fullsuccess.world@gmail.com>
2024-07-18 10:16:49 +0900

    modified: convert http to https

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,9 @@
 from app import app
 from app.utils.llm import prepare_llm
+from flup.server.fcgi import WSGIServer
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
+    WSGIServer(app, bindAddress=('0.0.0.0', 8080), certfile='cert.pem', keyfile='key.pem').run()
     prepare_llm()
