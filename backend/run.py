67871e797c5708cc28f1c97b6e7c9f060414feff
ethtop90 <fullsuccess.world@gmail.com>
2024-07-24 09:10:07 +0900

    added: insert project_mode to env file

--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,16 @@
 from app import app
 from app.utils.llm import prepare_llm
+import os
 # from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
+    mode = os.environ.get("project_mode")
     
     prepare_llm()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    if mode is "develop":
+        app.run(host='0.0.0.0', port=8080, debug=True)
+    elif mode is "production":
+        app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
+
     # http_server = WSGIServer(('0.0.0.0', 8080), app, keyfile='key.pem', certfile='cert.pem')
     # http_server.serve_forever()
